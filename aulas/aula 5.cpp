921ba3994c5ecc058e7553f8525f8b557a9b75d9
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2022-09-15 10:28:11 -0300

    Create aula 5.cpp

---
+++ aulas/aula 5.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main (){
+int idade[5];
+int i;
+
+		for(i = 0; i<5; i++ ){
+		
+	
+		printf ("digite a idade do eleitor %d: ", i+1) ;
+		scanf ("%i", &idade[i]);
+	
+}
+			for(i = 0; i<5; i++ ){
+				
+			
+				printf ("idade do eleitor %d: %i\n",i+1, idade[i]);
+	
+	
+}
+}
