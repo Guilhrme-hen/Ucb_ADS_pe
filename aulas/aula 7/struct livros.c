db48e0f1f2646d25d9e9e2868d661e7caed1d913
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2022-10-06 10:34:23 -0300

    Create struct livros.c

---
+++ aulas/aula 7/struct livros.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+struct livro{
+	char nome[30],editora[30];
+	float valor;
+	int volume;
+	};
+	
+int main (){		
+	
+	struct livro livro1, livro2;
+			
+		printf ("Digite o nome do livro: ")
+		scanf("%s", &livro1.nome);
+			
+		printf ("Digite a editora livro: ")
+		scanf("%s", &livro1.editora);
+			
+		printf ("Digite valor do livro: ")
+		scanf("%s", &livro1.valor);
+		
+		printf ("Digite volume do livro: ")
+		scanf("%s", &livro1.volume);
+		
+		//livro2
+		
+			printf ("Digite o nome do livro: ")
+			scanf("%s", &livro2.nome);
+			
+			printf ("Digite a editora livro: ")
+			scanf("%s", &livro2.editora);
+			
+			printf ("Digite valor do livro: ")
+			scanf("%s", &livro2.valor);
+		
+			printf ("Digite volume do livro: ")
+			scanf("%s", &livro2.volume);
+	
+		
+	
+	
+	
+	
+	
+	}
+	
+	
+	
